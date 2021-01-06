d6f6aa07df18f4ab184bdbbfc3960036f2a27e19
Christy Lam <cyyllam@gmail.com>
2021-01-05 22:32:15 -0800

    added bar graph sections and section headers

--- content/my_ggplot_script.R
+++ content/my_ggplot_script.R
@@ -7,13 +7,70 @@ library(extrafont) # optional
 # - on Windows use forwardslashes
 setwd("C:/Users/clam/Documents/github/intro-ggplot2/data")
 
-df <- read.xlsx("ofm_april1_population_final_tidied.xlsx", detectDates = T)
+df <- read.xlsx("ofm_april1_population_final_tidied.xlsx", detectDates = TRUE)
 
 
 # Bar Graph: Total Population by County and Year --------------------------
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Other geoms -------------------------------------------------------------
+
+
+
+
  
+
+
+
+
+
+
+
+
+# Facets ------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Extensions --------------------------------------------------------------
+
+
+
 
 
